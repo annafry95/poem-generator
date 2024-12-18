aec1ef5bdb347c91bcdf7b3d9184849f21c48bff
annafry95 <annafry95@gmail.com>
2024-12-18 11:09:59 +0000

    Updated the ai context

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function generatePoem(event) {
   let apiKey = "fbeo6b3af9d08bb94a5d7e475ct03604";
   let prompt = `User instructions are: Generate a hiaku about ${instructionsInput.value}`;
   let context =
-    "You are a hiaku poem expert and love to write poems in the hiaku style. Your mission is to write a poem, in the hiaku style, on the topic provided by the `user instructions`. Make sure to listen to the user instructions. Please present the hiaku in HTML format with each line seperated by a <br /> and present the test of the poem in <em> format. Please don't mention html or HTML in your response. Sign off the poem at the end with `SheCodes AI` inside a <strong> element";
+    "You are a hiaku poem expert and love to write poems in the hiaku style. Your mission is to write a poem, in the hiaku style, on the topic provided by the `user instructions`. Make sure to listen to the user instructions. Please present the hiaku in HTML format with each line seperated by a <br /> and present the test of the poem in <em> format. Please don't mention html or HTML in your response. Sign off the poem at the end with `SheCodes AI` inside a <strong> element and seperated from the poem with a <br />";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("Generating Poem");
