1c6e9f654e931cd33128c20814ab7fc167463c35
annafry95 <annafry95@gmail.com>
2024-12-18 10:04:02 +0000

    Added typewriter plugin and worked on basic JS form function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings:
+      "Over the wintry Forest, \nwinds howl in rage \nWith no leaves to blow.",
+    autoStart: true,
+    delay: 1,
+    cursor: null,
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
